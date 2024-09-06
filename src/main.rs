4baa34ef541ccbf446bb4f2916b878ccb899a92f
Matthew Billman <mgbvox@gmail.com>
2024-09-06 13:00:17 -0400

    start work on jitter/packet loss

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,39 @@
 use clap::{Parser, Subcommand};
-use std::net::TcpStream;
-use std::time::{Duration, Instant};
-use std::thread::sleep;
 use std::io;
+use std::net::{SocketAddr, TcpStream, ToSocketAddrs};
+use std::thread::sleep;
+use std::time::{Duration, Instant};
+
+const SERVER_DEFAULT: &str = "1.1.1.1:443";
+
+
+#[derive(Parser)]
+#[command(name = "netmon")]
+#[command(about = "A simple network stability monitoring tool", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Measure Round-Trip Time (RTT) to a server
+    Rtt {
+        /// Server to ping (default: cloudflare dns)
+        #[arg(default_value = SERVER_DEFAULT)]
+        server: String,
+
+        /// Interval in milliseconds between pings
+        #[arg(short, long, default_value_t = 1000)]
+        interval: u64,
+
+        /// Interval in milliseconds between pings
+        #[arg(short, long, default_value_t = 500)]
+        timeout: u64,
+    },
+    Version,
+}
+
 
 // This will set VERSION based on the build mode
 #[cfg(debug_assertions)]
@@ -21,18 +52,19 @@ fn format_server_address(server: &str) -> String {
     server
 }
 
-fn measure_rtt(server: &str) -> io::Result<Duration> {
+fn measure_rtt_w_timeout(server: &str, timeout: u64) -> Result<Duration, io::Error> {
     let formatted_server = format_server_address(server);
+    let addr = formatted_server
+        .to_socket_addrs()?
+        .next()
+        .unwrap_or_else(|| {
+            println!("Invalid server address: {}\nUsing default address {}", &formatted_server, &SERVER_DEFAULT);
+            SocketAddr::V4("1.1.1.1:443".parse().unwrap())
+        });
     let start = Instant::now();
-
-    // Try to establish a TCP connection
-    match TcpStream::connect(&formatted_server) {
-        Ok(_) => {
-            let duration = start.elapsed();
-            Ok(duration)
-        }
-        Err(e) => Err(e),
-    }
+    let _ = TcpStream::connect_timeout(&addr, Duration::from_millis(timeout))?;
+    let duration = start.elapsed();
+    Ok(duration)
 }
 
 fn main() {
@@ -42,11 +74,11 @@ fn main() {
         Commands::Version => {
             println!("{} version: {}", env!("CARGO_PKG_NAME"), VERSION);
         }
-        Commands::Rtt { server, interval } => {
+        Commands::Rtt { server, interval, timeout } => {
             let ping_interval = Duration::from_millis(*interval);
 
             loop {
-                match measure_rtt(server) {
+                match measure_rtt_w_timeout(server, timeout.clone()) {
                     Ok(duration) => println!("RTT to {}: {:?}", server, duration),
                     Err(e) => eprintln!("Failed to ping {}: {}", server, e),
                 }
@@ -54,27 +86,4 @@ fn main() {
             }
         }
     }
-}
-
-#[derive(Parser)]
-#[command(name = "netmon")]
-#[command(about = "A simple network stability monitoring tool", long_about = None)]
-struct Cli {
-    #[command(subcommand)]
-    command: Commands,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    /// Measure Round-Trip Time (RTT) to a server
-    Rtt {
-        /// Server to ping (default: google.com:443)
-        #[arg(default_value = "google.com:443")]
-        server: String,
-
-        /// Interval in milliseconds between pings
-        #[arg(short, long, default_value_t = 1000)]
-        interval: u64,
-    },
-    Version,
 }
